a4070f76096e5d67c5269f9aabf6e908c5d402df
ionpascaru <iionpascaru@gmail.com>
2020-01-11 17:10:34 +0100

    first exercise done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,14 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import IdCard from "./components/IdCard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>IdCard</h1>
-        {/* TODO: Use the IdCard component */}
+
+        <IdCard
+          lastName="Doe"
+          firstName="John"
+          gender="male"
+          height={178}
+          birth={new Date("1992-07-14")}
+          picture="https://randomuser.me/api/portraits/men/44.jpg"
+        />
+
+        <IdCard
+          lastName="Delores "
+          firstName="Obrien"
+          gender="female"
+          height={172}
+          birth={new Date("1988-05-11")}
+          picture="https://randomuser.me/api/portraits/women/44.jpg"
+        />
 
         <h1>Greetings</h1>
         {/* TODO: Use the Greetings component */}
+
+        <h1>Random</h1>
+
+        <h1>CreditCard</h1>
+
+        <h1>Rating</h1>
+
+        <h1>DriverCard</h1>
+
+        <h1>ClickablePicture</h1>
+
+        <h1>Dice</h1>
+
+        <h1>Carousel</h1>
+
+        <h1>NumbersTable</h1>
       </div>
     );
   }
